1f7f1fce96af1f04c58e1612deed94c51f02b112
Alexander <64682381+Alexander2990@users.noreply.github.com>
2022-01-22 16:40:35 +0500

    Add files via upload

---
+++ BMI-Calculator/BMI-Calculator-LayoutPractice/Models/BMI-Calculator-LayoutPractice/Models/Models/BMI.swift
@@ -0,0 +1,18 @@
+//
+//  BMI.swift
+//  BMI-Calculator-LayoutPractice
+//
+//  Created by Александр on 22.01.2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct BMI {
+    
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+}
